b4cd0d79382ffa9453788546fbac7d3a2bee76cc
anand651 <kumaranandstudent@gmail.com>
2024-06-16 17:03:27 +0530

    useEffect,useCallback,useRef

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
         >
           <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
             <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-              <button onClick={()=>setColor("red")} 
-              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              
+              <button onClick={()=>setColor("red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
               style={{background:"red"}}>red</button>
 
               <button onClick={()=>setColor("blue")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
