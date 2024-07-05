db3f82bd175105746c792b4c4af2442c8940dc48
anand651 <kumaranandstudent@gmail.com>
2024-07-06 00:27:05 +0530

    Hover rating star

---
+++ starRating/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import {FaStar} from 'react-icons/fa'
+
+function App({noOfstar=5}) {
+  const [rating,setRating] = useState(0)
+  const [hover,setHover]=useState(0)
+
+  function handleClick(getCurrentIndex){
+    setRating(getCurrentIndex)
+  }
+  function handleMouseMove(getCurrentIndex){
+   setHover(getCurrentIndex)
+  }
+  function handleMouseLeave(){
+  setHover(rating)
+  }
+  return (
+    <div className='star'>
+     {[...Array(noOfstar)].map((_,index) =>{
+       index +=1
+       return(
+        <FaStar
+          key={index}
+          className={index <= (rating||hover)?'active':'inactive'}
+          onClick={()=>handleClick(index)}
+          onMouseMove={()=>handleMouseMove(index)}
+          onMouseLeave={()=>handleMouseLeave()}
+          size={40}
+        />
+       )
+     })}
+    
+    </div>
+  )
+}
+
+export default App
