1cff7bf61e15e96f84b4e963525231b377ca53fd
Anand Kumar <kumaranandstudent@gmail.com>
2024-12-10 00:03:07 +0530

    React.StrictMode render two times

--- imageLoder/src/main.jsx
+++ imageLoder/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode> we can comment it because it can render the data. 
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
